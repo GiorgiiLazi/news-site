dea29bba651e15b33e8f3755b6dfa4903c661f8d
george <malazi140@gmail.com>
2023-05-22 16:30:33 +0300

    deployment of website

--- deploy.sh
+++ deploy.sh
@@ -15,9 +15,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:GiorgiiLazi/news-site.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GiorgiiLazi/news-site.git main:gh-pages
+# git push -f git@github.com:GiorgiiLazi/news-site.git main:gh-pages
 
 cd -
